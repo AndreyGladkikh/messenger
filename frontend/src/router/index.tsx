106847dbdb284f7daf265820a862ea2b25cee1e1
Гладких Андрей Игоревич <andrgladkikh@gmail.com>
2026-08-07 10:17:32 +0700

    feat(frontend): add theme provider

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
-import App from "../App";
-import Test from "../Test"
+import App from "@/App";
+import Test from "@/Test"
 
 export default createBrowserRouter([
     {
